e823ddf7380070f663b6341a48684b5e3bfbf2dc
MatheusFelipeRuiz <matheus57.ruiz100@gmail.com>
2023-10-12 11:33:15 -0300

    criação classes domínio

---
+++ src/main/java/com/example/bookstore/domain/Book.java
@@ -0,0 +1,72 @@
+package com.example.bookstore.domain;
+
+import java.util.Objects;
+
+public class Book {
+	private Integer id;
+	private String title;
+	private String authorName;
+	private String text;
+
+	public Book() {
+		super();
+	}
+
+	public Book(Integer id, String title, String authorName, String text) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.authorName = authorName;
+		this.text = text;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthorName() {
+		return authorName;
+	}
+
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
---
+++ src/main/java/com/example/bookstore/domain/Category.java
@@ -0,0 +1,73 @@
+package com.example.bookstore.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Category {
+	private Integer id;
+	private String name;
+	private String description;
+	private List<Book> listBooks = new ArrayList<>();
+
+	public Category() {
+
+	}
+
+	public Category(Integer id, String name, String description) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<Book> getListBooks() {
+		return listBooks;
+	}
+
+	public void setListBooks(List<Book> listBooks) {
+		this.listBooks = listBooks;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
